780b30053c54f1159a31189ee3bb2f67d7f48ef8
Polyxen1a <maslowa@inbox.ru>
2023-07-15 23:38:56 +0300

    Initial commit

---
+++ src/main/java/com/example/hibernateintroductionentityrepetition/HibernateIntroductionEntityRepetitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernateintroductionentityrepetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateIntroductionEntityRepetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateIntroductionEntityRepetitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hibernateintroductionentityrepetition/HibernateIntroductionEntityRepetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hibernateintroductionentityrepetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateIntroductionEntityRepetitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
